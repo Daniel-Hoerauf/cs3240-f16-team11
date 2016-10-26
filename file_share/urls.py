f8c654261af7bbfccc8e756810bdde733f492992
Daniel Hoerauf <dah4pe@virginia.edu>
2016-10-25 20:51:11 -0400

    Added basic required login functionality

--- file_share/urls.py
+++ file_share/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'file_share.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('web.urls'))
 ]
