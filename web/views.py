5cfae79f05fbf13675e843f09fd0f84c7879d23e
Daniel Hoerauf <dah4pe@virginia.edu>
2016-10-30 17:11:52 -0400

    Gracefully handle errors on registration

--- web/views.py
+++ web/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.messages import error
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 from django.views.decorators.http import require_http_methods
 
 @login_required
@@ -18,7 +20,11 @@ def create_account(request):
     username = request.POST.get('username')
     password = request.POST.get('password')
     email = request.POST.get('email')
-    user = User.objects.create_user(username, password, email)
+    try:
+        user = User.objects.create_user(username, password, email)
+    except IntegrityError:
+        error(request, 'That username has already been taken')
+        return redirect('/register/')
     user.set_password(password)
     user.save()
     user = authenticate(username=username, password=password)
