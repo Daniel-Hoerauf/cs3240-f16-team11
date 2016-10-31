d7997087f8cfdb873e89799df9e3e777fefb2506
Daniel Hoerauf <dah4pe@virginia.edu>
2016-10-31 17:18:18 -0400

    Fixed import error in urls.py

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^register/$', views.register_page, name='register_page'),
     url(r'^create_account/$', views.create_account, name='create_account'),
-    url(r'/report/form.html', 'auth_views.report_form_upload', name='report_form_upload'),
+    url(r'/report/form.html', views.report_form_upload, name='report_form_upload'),
 ]
