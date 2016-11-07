9173cbd5169e0485da52877d3805617eee637f2a
mhn94 <maggiehn94@yahoo.com>
2016-11-07 14:10:20 -0500

    added some bootstrap to login and registration pages

--- file_share/settings.py
+++ file_share/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web',
+    'bootstrap3',
+    'jquery',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -108,5 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
