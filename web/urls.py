d65ee45ae66c75b18ce9f98bee94a0a19be8ee7d
mhn94 <maggiehn94@yahoo.com>
2016-11-21 00:21:32 -0500

    site managers can make other existing users site managers

--- web/urls.py
+++ web/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^register/$', views.register_page, name='register_page'),
     url(r'^create_account/$', views.create_account, name='create_account'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^groups/$', views.groups, name='groups'),
     url(r'^create_group/$', views.create_group, name='create_group'),
     url(r'^group/$', views.group, name='group'),
     url(r'^add_member/$', views.add_member, name='add_member'),
+    url(r'^site_manager/$', views.site_manager, name='site_manager'),
+    url(r'^give_SM_status/$', views.give_SM_status, name='give_SM_status'),
 ]
