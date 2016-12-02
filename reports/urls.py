0751c7a8a1d182b58e4739ccee560c49df3bff4d
Daniel Hoerauf <dah4pe@virginia.edu>
2016-12-01 21:37:02 -0500

    Unique upload dest for each file

--- reports/urls.py
+++ reports/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.add_report, name='add_report'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^thanks/$', views.thanks, name='thanks'),
     url(r'^see/$', views.see_reports, name='see')
 
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Report
 from django.template import loader
@@ -8,21 +9,14 @@ from web.models import UserGroup
 from django.contrib.auth.models import User
 # Create your views here.
 
-def index(request):
-    return render(request, 'createReport.html')
-def thanks(request):
-    return render(request, 'form.html')
 
-# def create(request):
-
-    # return render(request, 'createReport.html', {'form': form_class,})
-
+@login_required
 def add_report(request):
     form_class = ReportForm(user=request.user)
     # if this is a POST request process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = ReportForm(request.POST, user=request.user)
+        form = ReportForm(request.POST, request.FILES, user=request.user)
         # check whether it's valid:
         if form.is_valid():
             report = form.save(commit=False)
@@ -41,6 +35,12 @@ def add_report(request):
 
     return render(request, 'createReport.html', {'form': form_class})
 
+
+def get_file_dest(instance, filename):
+    return 'user_{}/{}/{}/{}'.format(instance.owner.pk, instance.timestamp.minute,
+                                     instance.timestamp.second, filename)
+
+@login_required
 def see_reports(request):
     query = request.GET.get('search', '')
     template = loader.get_template('see_reports.html')
@@ -51,3 +51,14 @@ def see_reports(request):
     output = ', '.join([r.title for r in reports_list])
     context = RequestContext(request, {'reports_list': reports_list})
     return HttpResponse(template.render(context))
+
+
+@login_required
+def download_file(request, pk):
+    report = get_object_or_404(Report, pk=pk)
+    if report.group is not None:
+        if report.group not in UserGroup.objects.filter(members=request.user):
+            return HttpResponse(status=404)
+    response = HttpResponse(report.file, content_type='text/plain')
+    # response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    return response
